03552843d3a709adf159a26a99ebb23de3308165
Pritam Das <pritam.das.santu@gmail.com>
2023-12-07 01:13:06 +0530

    Initial commit

---
+++ week1-assignments/easy/anagram.js
@@ -0,0 +1,17 @@
+/*
+  Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
+  What's Anagram?
+  - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
+*/
+
+function isAnagram(str1, str2) {
+    let temp1=str1.toLowerCase();
+    let temp2=str2.toLowerCase();
+    const string1=temp1.split("").sort().join("");
+    const string2=temp2.split("").sort().join("");
+    if(string1==string2)
+      return true;
+    return false;
+}
+
+module.exports = isAnagram;
---
+++ week1-assignments/medium/countVowels.js
@@ -0,0 +1,22 @@
+/*
+  Implement a function `countVowels` that takes a string as an argument and returns the number of vowels in the string.
+  Note: Consider both uppercase and lowercase vowels ('a', 'e', 'i', 'o', 'u').
+
+  Once you've implemented the logic, test your code by running
+*/
+function isVowel(str){
+  if(str=='a' || str=='e' || str=='i' || str=='o' || str=='u' || str=='A' || str=='E' || str=='I' || str=='O' || str=='U')
+    return true;
+  return false;
+}
+function countVowels(str) {
+    // Your code here
+    let count=0;
+    for(let i=0; i<str.length; i++){
+      if(isVowel(str[i]))
+        count++;
+    }
+    return count;
+}
+
+module.exports = countVowels;
