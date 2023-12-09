39883dfe1337c938373f0266367f3ae3ddcf030a
Pritam Das <pritam.das.santu@gmail.com>
2023-12-09 23:37:55 +0530

    Update 2-clock.js

--- week2-assignments/medium/2-clock.js
+++ week2-assignments/medium/2-clock.js
@@ -3,6 +3,18 @@ setInterval(()=>{
     let hours=date.getHours();
     let minutes=date.getMinutes();
     let seconds=date.getSeconds();
-    let twelve_hours=
+    let zone;
+    let actualHours;
+    if(hours>=0 && hours<=11)
+        zone='AM';
+    else
+        zone='PM';
+    if(hours==12){
+        actualHours=12;
+    }
+    else{
+        actualHours=hours%12;
+    }
     console.log(`${hours}:${minutes}:${seconds}`);
+    console.log(`${actualHours}:${minutes}:${seconds} `+zone);
 }, 1000);
